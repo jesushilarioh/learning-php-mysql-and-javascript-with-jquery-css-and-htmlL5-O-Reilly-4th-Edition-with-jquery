eddb226dedbdd9b530345aae3364d00f15a33df6
Jesus Hilario <jesushilariohernandez@gmail.com>
2017-08-27 22:46:15 -0500

    added

---
+++ 4th_edition_examples/3 - Introduction to PHP/example3-5_b_String-Concatenation.php
@@ -0,0 +1,12 @@
+<?php
+  // STRING CONCATENATION
+  // use the perido (.) to append one string to another
+  $msgs = 5
+  echo "You have " . $msgs . " messages.";
+  // You can also use .=
+  $bulletin = "Bulletin says: "
+  $newsflash = "What up buddy?"
+  $bulletin .= $newsflash;
+
+  
+?>
---
+++ 4th_edition_examples/3 - Introduction to PHP/example3-5_c_String-Types.php
@@ -0,0 +1,7 @@
+<?php
+  // STRING TYPES
+  // Single quotes ('') == Used when you wish to assign a literal string
+  $info = 'Preface variables with a $ like this: $variable';
+  // Double quotes ("") == When including the value of a variable within a string
+  echo "This week $count people have viewed your profile"; // Referred to as variable substitution
+?>
